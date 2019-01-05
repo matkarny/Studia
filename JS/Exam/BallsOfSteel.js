1b2374d7d9039231d85f96d66980b2344224a51b
matkarny <44334084+matkarny@users.noreply.github.com>
2019-01-05 09:23:10 +0100

    1 Exam app

--- JS/Exam/BallsOfSteel.js
+++ JS/Exam/BallsOfSteel.js
@@ -33,43 +33,32 @@ function theBiggestBall(weight,addition) {
             weight: weight
         },
     ];
-    ballsOfSteel[parseInt(8 * Math.random())].weight += addition;
-    if (ballsOfSteel.length === 8) {
-        let arr1 = ballsOfSteel;
-        let arr2 = [];
-        while (arr1.length > 4) {
-            arr2.push(arr1.pop())
+    ballsOfSteel[parseInt(8 * Math.random())].weight += addition; //adding weight to random ball
+ // Counting module until we find final pair
+   if(ballsOfSteel.length ==! 2) {
+       if (ballsOfSteel.length === ballsOfSteel.length/2) {
+           let arr1 = ballsOfSteel;
+           let arr2 = [];
+           while (arr1.length > ballsOfSteel.length/2) {
+               arr2.push(arr1.pop())
 
-        }
-        let firstWeight = 0;
-        arr1.forEach(i => firstWeight += i.weight);
-        if (firstWeight === 4*weight) {
-            ballsOfSteel = arr2
-        } else {
-            ballsOfSteel = arr1
-        }
-    }
-    if (ballsOfSteel.length === 4) {
-        let arr3 = ballsOfSteel;
-        let arr4 = [];
-        while (arr3.length > 2) {
-            arr4.push(arr3.pop())
+           }
+           let firstWeight = 0;
+           arr1.forEach(i => firstWeight += i.weight);
+           if (firstWeight === 4*weight) {
+               ballsOfSteel = arr2
+           } else {
+               ballsOfSteel = arr1
+           }
+       }
+   } else {
 
-        }
-        let secondWeight = 0;
-        arr3.forEach(x => secondWeight += x.weight);
-        if (secondWeight === 2*weight) {
-            ballsOfSteel = arr4
-        } else {
-            ballsOfSteel = arr3
-        }
-    }
     if (ballsOfSteel[0].weight === weight) {
         console.log(`The biggest ball of steel is ${ballsOfSteel[1].name} with weight ${ballsOfSteel[1].weight}`)
     } else {
         console.log(`The biggest ball of steel is ${ballsOfSteel[0].name} with weight ${ballsOfSteel[0].weight}`)
     }
 }
+}
 
-
-theBiggestBall(40,5);
+theBiggestBall(50,10);
